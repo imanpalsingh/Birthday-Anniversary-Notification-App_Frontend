e24208a6cda22b2b8e84b05c0370eda83bf369c7
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2021-04-25 21:14:19 +0530

    convert to func comp; refactored

--- src/App.js
+++ src/App.js
@@ -1,72 +1,39 @@
-import Auth from "./AuthService/Auth";
 import Home from "./components/Home/Home.component";
 import PrivateRoute from "./AuthService/PrivateRoute/PrivateRoute.Route";
-import React, {Component} from "react";
+import React, {useState} from "react";
 import SignIn from "./components/SignIn/SignIn.component";
-import { Switch, Route, BrowserRouter as Router} from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router, Redirect} from "react-router-dom";
 
-class App extends Component {
-
-  constructor(props){
-    super(props);
-
-    //State for global user authentication
-    this.state={
-      username:"",
-      password:"",
-      token:"" //jwt
-    }
-
-  }
+function App(){
 
   /*
-  Function makes an api call to receive token using username and password
+      signed determines if the current session has a authedcated user or not
+      updateSigned is drilled down as props to update it at various children
   */
-  updateToken = async (event)=>{
-    const token = await Auth.signIn(this.state.username,this.state.password)
-    this.setState({token:token})
-  }
-
-  componentDidMount(){
-    const token = localStorage.getItem("token")
-    this.setState({
-      token:token
-    })
-  }
-
-  /*
-     This binded function for child components; for updating states
-  */
-  updateAuth = (event)=>{
+  const [signed,updateSigned] = useState(false);
     
-    event.preventDefault();
-    this.setState({
-      username: event.target.usernameText.value,
-      password: event.target.passwordText.value
-    },()=>{
-      this.updateToken()
-    })
-    
-    
-  }
-
-  render(){
-   
-    return (
+  return (
       <div className="App">
         <Router>
           <Switch>
             <Route exact path="/">
-              <PrivateRoute Component={Home} signOut={Auth.signOut} Display={this.state.token?true:false}/>
+              <PrivateRoute Component={Home} display={signed} updateDisplay={updateSigned}/>
             </Route>
             <Route exact path="/signin"> 
-              <PrivateRoute Component={SignIn} Display={this.state.token?false:true} FallbackLink="/" updateAuth={this.updateAuth}/>
+              {
+                /*
+                  Conditional redering moved here to simplify the signin component
+                */
+                signed?
+                <Redirect to="/" />:
+                <SignIn signed={signed}  updateSigned={updateSigned}/>
+
+              } 
             </Route>
           </Switch>
         </Router>
       </div>
     )
-  }
 }
 
 export default App;
