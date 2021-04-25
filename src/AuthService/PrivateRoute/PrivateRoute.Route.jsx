f9ffcc61c45c51f8df2cf75e563ccf850f098f2f
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2021-04-25 21:15:57 +0530

    Move client side authentication to seperate file

--- src/AuthService/PrivateRoute/PrivateRoute.Route.jsx
+++ src/AuthService/PrivateRoute/PrivateRoute.Route.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-
+import { useHistory } from 'react-router-dom';
+import Auth from "../Auth";
 
 /*
-    PrivateRouter accepts Components which are to be only conditionaly rendered
+    PrivateRouter accepts Components which are to be only conditionaly rendered based on token verification from server
 
     Component : Component to display
-    Display : (bool) To display the component or to redirect
-    FallbackLink : If display is false redirect to this link
-    ComponentProps : Additional props to pass to the component
-*/
-const PrivateRoute = ({Component, Display, FallbackLink="/signin", ...ComponentProps})=>{
+    
+    display : the hook state that determines if the component is to be dispplayed 
+    [After it is updated in this function, old values do not contribute ]
 
-    if(!Display){
-        return (<Redirect to={FallbackLink} />)
+    updateDisplay : the valid hook updator for display
+    
+    fallbackLink : If display is false redirect to this link
+    
+    componentProps : Additional props to pass to the component
+*/
+const PrivateRoute = ({Component, display, updateDisplay,  fallbackLink="/signin", ...componentProps})=>{
+
+
+    const history = useHistory();
+    const token = localStorage.getItem("token");
+    
+    // If no token exists goto fallback
+    if(!token) {history.push(fallbackLink); return}
+
+    // Validate the token.
+    Auth.getDatawithToken(token, updateDisplay);
+
+    // if token was valid, it had updated the display hook (via dataUpdator). If not then fallback
+    if(!display){
+        history.push(fallbackLink)
     }
 
+    //the original component [Also drilling down the app.js states for future use]
     return(
-        <Component {...ComponentProps} />
+        <Component {...componentProps} signed={display} updateSigned={updateDisplay} />
     )
 }
 
